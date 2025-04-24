448815360397fb4b764d73b232e74e922861c09f
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-04-24 11:36:36 -0300

    Feat: cache and Priority

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import styles from "./Home.module.css";
 import CharacterCard from "../../components/CharacterCard";
+import Loader from "../../components/Loader";
 
 export default function Home() {
     const [search, setSearch] = useState("");
@@ -14,38 +14,107 @@ export default function Home() {
     const [notFound, setNotFound] = useState(false);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
+    const [loading, setLoading] = useState(true);
+    const cacheRef = useRef(new Map());
 
     
-        const fetchCharacters = async (name, pageNumber) => {
+        const fetchCharacters = async (name="", pageNumber=1) => {
+            setLoading(true);
+            const cache = cacheRef.current;
+            const cacheKey = `${name}_${pageNumber}`;
+            const nextPageNumber = pageNumber + 1;
+            const nextCacheKey = `${name}_${nextPageNumber}`;
+
+            const cleanCacheIfNeeded = () => {
+                while (cache.size >= 5) {
+                    const firstKey = cache.keys().next().value;
+                    cache.delete(firstKey);
+                    console.log(`⛔ Página removida do cache: ${firstKey}`);
+                }
+        };
+
+        console.log("\n============== BUSCA INICIADA ==============");
+        console.log(`◀ Cache anterior: ${cache.size} páginas`);
+
+        let total = totalPages;
+        if (cache.has(cacheKey)) {
+            const cached = cache.get(cacheKey);
+            setCharacters(cached.results);
+            setTotalPages(cached.totalPages);
+            total = cached.totalPages;
+            setNotFound(false);
+            setLoading(false);
+            console.log(`☑ Cache sendo utilizado: ${cacheKey}`);
+        } else {
             try {
                 const { data } = await axios.get(`https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${name}`);
+
+                cleanCacheIfNeeded();
+                cache.set(cacheKey, {
+                    results: data.results,
+                    totalPages: data.info.pages,
+                }); // salvar no cache
+
                 setCharacters(data.results);
                 setTotalPages(data.info.pages);
+                total = data.info.pages;
                 setNotFound(false);
+                console.log(`💾 Salvo no cache: ${cacheKey}`);
             } catch {
-                setNotFound(true);
                 setCharacters([]);
+                setNotFound(true);
+            } finally {
+                setLoading(false);
             }
+        }
+
+        if (nextPageNumber <= total && !cache.has(nextCacheKey)) {
+            try {
+                const res = await axios.get(`https://rickandmortyapi.com/api/character/?page=${nextPageNumber}&name=${name}`);
+                cleanCacheIfNeeded();
+                cache.set(nextCacheKey, {
+                    results: res.data.results,
+                    totalPages: res.data.info.pages,
+                });
+                console.log(`💚 Prefetch salvo: ${nextCacheKey}`);
+            } catch (err) {
+                console.log(`💔 Prefetch falhou: ${nextCacheKey}`, err);
+            }
+        } else {
+            console.log("💥 Prefetch ignorado: já no cache ou fora do limite");
+        }
+
+        console.log(`🎁 Cache final: ${cache.size} páginas`);
+        for (const [key, val] of cache.entries()) {
+            console.log(`🎁 ${key}: ${val.results.length} personagens`);
+        }
+        console.log("============== FIM DA BUSCA ==============\n");
         };
 
         useEffect(() => {
-            fetchCharacters(search.trim(), page);
+            fetchCharacters();
+        }, []);
+
+        useEffect(() => {
+            fetchCharacters(search, page);
         }, [page]);
 
         const handleCardClick = (name) => {
             toast.info(`Você clicou no personagem: ${name}`, {});
         };
 
-        const handleResetClick = (message) => {
-            toast.info(message, {});
-        }
+        const handleResetClick = () => {
+            setSearch("");
+            setPage(1);
+            fetchCharacters("", 1);
+            toast.success("Filtro foi resetado", { position: "top-right" });
+        };
     
-        const handleSearchClick = (message) => {
-            toast.info(message, {
-            });
-        }
+        const handleSearchClick = () => {
+            setPage(1);
+            fetchCharacters(search, 1);
+        };
     
-
     console.log(characters);
 
     return(
@@ -76,12 +145,16 @@ export default function Home() {
             <div className={styles.navControls}>
                 <button
                 onClick={() => setPage((p) => Math.max(p - 1,1))}
-                disabled={page === 1}
+                disabled={page === 1 || notFound}
                 className={styles.buttonNav}
                 >Previous</button>
+
+                <span className={styles.pageInfo}>Page {page} of {totalPages}
+                </span>
+
                 <button
                 onClick={() => setPage((p) => Math.min(p + 1, totalPages))}
-                disabled={page === totalPages}
+                disabled={page === totalPages || notFound}
                 className={styles.buttonNav}
                 >Next</button>
             </div>
@@ -89,13 +162,18 @@ export default function Home() {
             {notFound && (
                 <h1 className={styles.notFound}>Nenhum personagem encontrado</h1>
             )}
-            
-            <div className={styles.grid}>
-            {characters.map((char) => (
-            <CharacterCard key={char.id} character={char} onClick={() => handleCardClick(char.name)} />
-           ))}
-            </div>
-           
+
+            {loading ? (
+                <div className={`${styles.loaderWrapper} ${loading ? "" : styles.hidden}`}>
+                    <Loader />
+                </div>
+            ) : (
+                <div className={styles.grid}>
+                    {characters.map((char) => (
+                        <CharacterCard key={char.id} character={char} onClick={() => handleCardClick(char)} />
+                    ))}
+                </div>
+            )}
         </div>
-   );
+    );
 }
